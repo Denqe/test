e31c49141abf5caf7421ae39413f39376e129228
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-03-29 00:05:38 +0300

    Create site.pp

---
+++ for 4homework/site.pp
@@ -0,0 +1,47 @@
+node 'slave1.puppet'{
+  package {'httpd':
+    ensure => installed,
+}
+  
+  service {'firewalld':
+    ensure => stopped,
+}
+  
+  file { '/var/www/html/index.html':
+    ensure => file,
+    source => '/vagrant/index.html'
+}
+   
+  service {'httpd':
+    ensure => running,
+}
+}
+
+node 'slave2.puppet'{
+  package {'httpd':
+    ensure => installed,
+}
+  
+  package {'php':
+    ensure => installed,
+}
+  
+  service {'firewalld':
+    ensure => stopped,
+} 
+  file {'/var/www/html/index.php':
+    ensure => file,
+    source => '/vagrant/index.php'
+}
+  
+  service {'httpd':
+    ensure => running,
+}  
+}
+
+node 'master.puppet'{
+  package {'nginx':
+    ensure => installed,
+}
+
+}
