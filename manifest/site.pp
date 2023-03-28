6fc382d02c7d5bcd7c8167901b1bf326777addba
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-03-28 17:11:48 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -7,8 +7,14 @@ file {'/etc/systemd/system/minecraft.service':
   source => '/vagrant/minecraft.service'
 }
 
-exec {'install':
+exec {'download':
   command => 'wget https://www.minecraft.net/ru-ru/download/server'
   cwd => '/opt/minecraft',
   require => File['/opt/minecraft'],
+}
+exec {'install':
+  command => 'java -Xmx1024M -Xms1024M -jar minecraft_server..jar nogui',
+  cwd => '/opt/minecraft',
+  #path =>
+  require => Exec['download'],
 }
